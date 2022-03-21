f193e26b01220509d2804c9c8a759579aa413cc4
Vaibhav360-web <patilvaibhu1656@gmail.com>
2022-03-21 19:56:58 +0530

    MODIFIED: Address book with contact info

--- src/Address_Book.java
+++ src/Address_Book.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class Address_Book {
+    Scanner sc = new Scanner(System.in);
+    Contact contact = new Contact();
+    public void setContact(){
+        System.out.println("Enter the First Name=");
+        contact.setFirstname(sc.next());
+        System.out.println("Enter the Last Month=");
+        contact.setLastname(sc.next());
+        System.out.println("Enter the Address");
+        contact.setAddress(sc.next());
+        System.out.println("Enter the City");
+        contact.setCity(sc.next());
+        System.out.println("Enter the State");
+        contact.setState(sc.next());
+        System.out.println("Enter the Phone Number");
+        contact.setPhoneno(sc.next());
+        System.out.println("Enter the Zip-Code");
+        contact.setZip(sc.next());
+        System.out.println("Enter the Email");
+        contact.setEmail(sc.next());
+    }
+    public void showContact(){
+        System.out.println(contact.toString());
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book System");
+        Address_Book address_book = new Address_Book();
+        address_book.setContact();
+        address_book.showContact();
     }
 }
---
+++ src/Contact.java
@@ -0,0 +1,88 @@
+public class Contact {
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String city;
+    private String state;
+    private String phoneno;
+    private String zip;
+    private String email;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", phoneno='" + phoneno + '\'' +
+                ", zip='" + zip + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
